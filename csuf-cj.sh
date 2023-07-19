a01611eb161bc2d9e20bae55232ad3e59a2105e7
cernLabs <mikpen.p@gmail.com>
2023-07-19 10:53:23 -0700

    regular backup for 2023-07-19

---
+++ csuf-cj.sh
@@ -0,0 +1,12 @@
+# the purpose of this program is to regulary push my CSUF to a personal repository on GH
+
+# go to correct dir
+cd /home/cern/Desktop/githubbahubba/CSUF
+
+# get date
+date=$(date +%Y-%m-%d)
+
+# upload
+git add .
+git commit -m "regular backup for $date"
+git push
